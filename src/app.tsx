7f5c97161d43ff2d557ad1463fd21cff8a611f09
Evgeny Bazhanov <evg.bazhanov@yahoo.com>
2021-01-30 14:16:01 +0100

    - init

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import Tickets from "app/views/main";
+
+const App: React.FC = () => (
+    <Tickets/>
+);
+
+export default App;
---
+++ src/app/components/page/index.ts
@@ -0,0 +1,3 @@
+export { default } from './page';
+
+require('./page.scss');
